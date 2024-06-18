8e126ed59f8c37b1de5d413cb034256ad15c6ad2
noir.desir.sublime@gmail.com <tuemailexample.com>
2024-06-18 14:26:30 -0400

    visualizacion en view,main y html

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { example } from './dataFunctions.js';
+//import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
-
 import data from './data/dataset.js';
 
-console.log(example, renderItems(data), data);
+function init() {
+// Mostrar los elementos en la interfaz de usuario
+  renderItems(data);
+}
+// Ejecutar la función de inicialización cuando el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', init);
--- src/view.js
+++ src/view.js
@@ -1,6 +1,32 @@
-export const renderItems = (data) => {
-  console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
-};
 
+export const renderItems = (data) => {
+  // Crear un elemento <ul>
+  const ul = document.createElement('ul');
+  // Recorrer la data y crear un elemento <li> para cada elemento de la data
+  data.forEach(item => {
+    const li = document.createElement('li');
+    // Crear el contenido del <li> con la información del item
+    const content = `
+          <img src="${item.imageUrl}" alt="${item.name}" style="width:100px;height:auto;">
+          <h2>${item.name} (${item.shortDescription})</h2>
+          <p>${item.description}</p>
+          <ul>
+              <li><strong>Fecha de Nacimiento:</strong> ${item.facts.fechaDeNacimiento}</li>
+              <li><strong>Signo Zodiacal:</strong> ${item.facts.signoZodiacal}</li>
+              <li><strong>Tipo de Guardiana:</strong> ${item.facts.tipoDeGuardiana}</li>
+          </ul>
+      `;
+    const rootElement = document.querySelector('#root');
+
+    // Renderizar los elementos dentro de 'root'
+    rootElement.appendChild(renderItems(data));
+
+    li.innerHTML = content;
+    // Agregar el <li> al <ul>
+    ul.appendChild(li);
+  });
+  // Retornar el elemento <ul>
+  return ul;
+}
+// Exportar la función para que pueda ser utilizada en otros archivos
+//export { renderItems };
